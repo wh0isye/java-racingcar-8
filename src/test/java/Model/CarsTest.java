46184624f666462e4bf1ca31254efa1e19c15071
Yaechan Lee <lyc021119@gmail.com>
2025-10-27 19:30:45 +0900

    test(model): Cars class Unit Test

---
+++ src/test/java/Model/CarsTest.java
@@ -0,0 +1,73 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class Carstest {
+
+    @Test
+    void testGetMaxPosition() {
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+        Car jun = new Car("jun");
+        Cars cars = new Cars(List.of(pobi, woni, jun));
+
+        pobi.move(4);
+        woni.move(4);
+        jun.move(4);
+
+        pobi.move(4);
+        woni.move(4);
+
+        pobi.move(4);
+
+        assertEquals(3, cars.getMaxPosition());
+
+    }
+
+    @Test
+    void testGetOneWinner() {
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+        Car jun = new Car("jun");
+        Cars cars = new Cars(List.of(pobi, woni, jun));
+
+        pobi.move(4);
+        woni.move(4);
+        jun.move(4);
+
+        pobi.move(4);
+        woni.move(4);
+
+        pobi.move(4);
+
+        assertEquals(List.of("pobi"), cars.getWinner());
+    }
+
+    @Test
+    void testGetMultipleWinner() {
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+        Car jun = new Car("jun");
+        Cars cars = new Cars(List.of(pobi, woni, jun));
+
+        pobi.move(4);
+        woni.move(4);
+        jun.move(4);
+
+        pobi.move(4);
+        woni.move(4);
+
+        pobi.move(4);
+        woni.move(4);
+
+        assertEquals(List.of("pobi", "woni"), cars.getWinner());
+    }
+}
+
+
+
