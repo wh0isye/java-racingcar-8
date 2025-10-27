33fe5e1ed68f8f13e2a1cf061a96449821e5e643
Yaechan Lee <lyc021119@gmail.com>
2025-10-27 19:37:50 +0900

    feat(model): add Exception Handling for Cars class

--- src/main/java/Model/Cars.java
+++ src/main/java/Model/Cars.java
@@ -6,8 +6,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Cars {
     private final List<Car> carList;
+    private static final int MAX_CAR_COUNT = 10;
+
+    public void checkCarCount(List<Car> carList) {
+        if(carList.size() > MAX_CAR_COUNT) {
+            throw new IllegalArgumentException("최대 자동차 수는 " + MAX_CAR_COUNT + " 입니다.");
+        }
+    }
 
     public Cars(List<Car> carList) {
+        checkCarCount(carList);
         this.carList = carList;
     }
 
