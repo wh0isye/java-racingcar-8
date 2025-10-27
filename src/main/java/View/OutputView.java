d0eb306aecf6770cbc4f7cfb2417f0fd42cfa6cd
Yaechan Lee <lyc021119@gmail.com>
2025-10-27 22:28:16 +0900

    feat(view): add OutputView class

---
+++ src/main/java/View/OutputView.java
@@ -0,0 +1,19 @@
+package View;
+
+import Model.Car;
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class OutputView {
+
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getCarName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printFinalWinner(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
